3c06146ee226a6d2b4cf5a413f5e38e63f307a8e
Silvia Cruz Román <scr00043@red.ujaen.es>
2024-10-02 17:06:10 +0200

    Creacion solicitud.java

---
+++ src/main/java/es/ujaen/dae/clubsocios/entidades/Solicitud.java
@@ -0,0 +1,81 @@
+package es.ujaen.dae.clubsocios.entidades;
+
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDate;
+
+
+public class Solicitud {
+
+    @Min(0) @Max(5)
+    private int nAcompanantes;
+    @NotBlank
+    private LocalDate fecha;
+    @NotBlank
+    private int insAceptadas;
+    @NotBlank
+    Socio  solicitante;
+
+    /**
+     * @brief Constructor por defecto de la clase solicitud
+     */
+    public Solicitud() {
+        this.nAcompanantes = 0;
+        this.fecha = LocalDate.now();
+        this.insAceptadas = 0;
+        this.solicitante = new Socio();
+    }
+
+
+
+
+    /**
+     * @brief Constructor parametrizado
+     * @param nAcompanantes numero de acompañantes
+     * @param fecha fecha en la que se realiza la solicitud
+     * @param insAceptadas numero de inscripciones aceptadas
+     * @param solicitante Socio que realiza la solicitud
+     */
+    public Solicitud(int nAcompanantes, LocalDate fecha, int insAceptadas, Socio solicitante) {
+        this.nAcompanantes = nAcompanantes;
+        this.fecha = fecha;
+        this.insAceptadas = insAceptadas;
+        this.solicitante = solicitante;
+    }
+
+    // getters y setters de los diferentes atributos
+    public int getnAcompanantes() {
+        return nAcompanantes;
+    }
+
+    public void setnAcompanantes(int nAcompanantes) {
+        this.nAcompanantes = nAcompanantes;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getInsAceptadas() {
+        return insAceptadas;
+    }
+
+    public void setInsAceptadas(int insAceptadas) {
+        this.insAceptadas = insAceptadas;
+    }
+
+    public Socio getSolicitante() {
+        return solicitante;
+    }
+
+    public void setSolicitante(Socio solicitante) {
+        this.solicitante = solicitante;
+    }
+}
