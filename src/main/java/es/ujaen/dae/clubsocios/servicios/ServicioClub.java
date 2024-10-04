f04d9cc1189150005673f292d25812b85a43e301
Silvia Cruz Román <scr00043@red.ujaen.es>
2024-10-04 13:18:20 +0200

    Añadir cabeceras ServicioClub

--- src/main/java/es/ujaen/dae/clubsocios/servicios/ServicioClub.java
+++ src/main/java/es/ujaen/dae/clubsocios/servicios/ServicioClub.java
@@ -1,4 +1,58 @@
 package es.ujaen.dae.clubsocios.servicios;
 
+import es.ujaen.dae.clubsocios.entidades.Actividad;
+import es.ujaen.dae.clubsocios.entidades.Socio;
+
+import java.util.Date;
+
 public class ServicioClub {
+    Socio login (String email,String clave){
+
+
+        return null;
+    }
+
+
+    Boolean anadirSocio(String nombre,String apellido,String telefono,String clave){
+        return null;
+    }
+
+    Boolean borrarSocio( String email){
+        return null;
+    }
+    Boolean anadirActividad(String titulo, String descripcion, double precio, int nPlazas, Date fechaCelebracion,Date fechaInscripcion){
+        return null;
+    }
+    Boolean borrarActividad(String titulo){
+        return null;
+    }
+    void revisarSolicitudes(){
+
+    }
+    void marcarCuotaPagada(Socio socio){
+
+    }
+    Actividad buscarActividad(String titulo,int anio){
+        return null;
+    }
+
+    Boolean realizarSolicitud(int nAcompanantes,Actividad actividad){
+        return  null;
+    }
+    Boolean anadirAcompanante(){
+        return null;
+    }
+    Boolean quitarAcompanante(){
+        return null;
+    }
+    void borrarSolicitud(Actividad actividad){
+
+    }
+    void pagarCuota(Socio socio){
+
+    }
+
+    void crearNuevaTemporada(Socio socio){
+
+    }
 }
