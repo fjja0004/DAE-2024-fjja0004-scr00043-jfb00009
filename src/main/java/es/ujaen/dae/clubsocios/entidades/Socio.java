78d2d57d4050c7e4218733873a69e2f27e8cc6d3
Francisco Javier Jiménez Aznar <fjja0004@red.ujaen.es>
2024-10-09 17:08:09 +0200

    Eliminados setters y getters no usados en Socio

--- src/main/java/es/ujaen/dae/clubsocios/entidades/Socio.java
+++ src/main/java/es/ujaen/dae/clubsocios/entidades/Socio.java
@@ -35,44 +35,12 @@ public class Socio {
         this.cuotaPagada = false;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
     public String getClave() {
         return clave;
-    }
-
-    public void setClave(String clave) {
-        this.clave = clave;
     }
 
     public boolean isCuotaPagada() {
