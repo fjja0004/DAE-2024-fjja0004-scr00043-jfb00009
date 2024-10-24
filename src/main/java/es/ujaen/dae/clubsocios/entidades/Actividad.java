a3721d7de06f0d84c0de25342e47ccaddf9952df
Francisco Javier Jiménez Aznar <fjja0004@red.ujaen.es>
2024-10-24 18:03:24 +0200

    Correcciones en Actividad
    Se ha creado un método llamado fechasValidas() que comprueba si las fechas de la actividad son válidas.

--- src/main/java/es/ujaen/dae/clubsocios/entidades/Actividad.java
+++ src/main/java/es/ujaen/dae/clubsocios/entidades/Actividad.java
@@ -4,6 +4,9 @@ import es.ujaen.dae.clubsocios.excepciones.*;
 import es.ujaen.dae.clubsocios.excepciones.SolicitudNoValida;
 import es.ujaen.dae.clubsocios.excepciones.SolicitudYaRealizada;
 import es.ujaen.dae.clubsocios.objetosValor.Solicitud;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -16,6 +19,9 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Actividad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     @NotBlank
     private String titulo;
     @NotBlank
@@ -37,6 +43,7 @@ public class Actividad {
      * @brief Constructor por defecto de la clase Actividad
      */
     public Actividad() {
+        this.id = 0;
         this.titulo = "";
         this.descripcion = "";
         this.precio = 0;
@@ -49,16 +56,17 @@ public class Actividad {
     }
 
     /**
-     * @param titulo String título de la actividad
-     * @param descripcion String con una breve descripción de la actividad
-     * @param precio Número entero de precio que cuesta la actividad
-     * @param plazas Número entero de plazas que tiene una actividad
-     * @param fechaCelebracion Fecha en la que se va a realizar la actividad
+     * @param titulo                 String título de la actividad
+     * @param descripcion            String con una breve descripción de la actividad
+     * @param precio                 Número entero de precio que cuesta la actividad
+     * @param plazas                 Número entero de plazas que tiene una actividad
+     * @param fechaCelebracion       Fecha en la que se va a realizar la actividad
      * @param fechaInicioInscripcion Fecha en la que inicio el periodo de inscripción
-     * @param fechaFinInscripcion Fecha en la que termina el periodo de inscripción
+     * @param fechaFinInscripcion    Fecha en la que termina el periodo de inscripción
      * @brief Constructor parametrizado de la clase Actividad
      */
     public Actividad(String titulo, String descripcion, int precio, int plazas, LocalDate fechaCelebracion, LocalDate fechaInicioInscripcion, LocalDate fechaFinInscripcion) {
+        this.id = 0;
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.precio = precio;
@@ -72,25 +80,25 @@ public class Actividad {
 
 
     /**
-     * @brief realiza una solicitud a la actividad
      * @param solicitud Solicitud
+     * @brief realiza una solicitud a la actividad
      */
     public void realizarSolicitud(@Valid Solicitud solicitud) {
 
-        if (!(solicitud.getFecha().isAfter(fechaInicioInscripcion) && solicitud.getFecha().isBefore(fechaFinInscripcion))){
-            if (solicitudesAceptadas.containsKey(solicitud.getSolicitante().getEmail())|| solicitudesPendientes.contains(solicitud))
+        if (!(solicitud.getFecha().isAfter(fechaInicioInscripcion) && solicitud.getFecha().isBefore(fechaFinInscripcion))) {
+            if (solicitudesAceptadas.containsKey(solicitud.getSolicitante().getEmail()) || solicitudesPendientes.contains(solicitud))
                 throw new SolicitudYaRealizada();
 
-            if (solicitudesAceptadas.size()<plazas && solicitud.getSolicitante().isCuotaPagada()){
-            solicitudesAceptadas.put(solicitud.getSolicitante().getEmail(),solicitud);
-                }
+            if (solicitudesAceptadas.size() < plazas && solicitud.getSolicitante().isCuotaPagada()) {
+                solicitudesAceptadas.put(solicitud.getSolicitante().getEmail(), solicitud);
+            }
             if (solicitudesAceptadas.size() >= plazas)
                 solicitudesPendientes.add(solicitud);
-            if (!solicitud.getSolicitante().isCuotaPagada()){
+            if (!solicitud.getSolicitante().isCuotaPagada()) {
                 solicitudesPendientes.add(solicitud);
             }
 
-        } else{
+        } else {
             throw new SolicitudNoValida();
         }
     }
@@ -98,7 +106,7 @@ public class Actividad {
     /**
      * @brief Devuelve una lista con las solicitudes pendientes
      */
-    public List<Solicitud> listaPendientes(){
+    public List<Solicitud> listaPendientes() {
         if (fechaFinInscripcion.isBefore(LocalDate.now())) {
             return solicitudesPendientes;
         }
@@ -106,7 +114,7 @@ public class Actividad {
     }
 
     /**
-     * @param solicitante socio que realiza la solicitud
+     * @param solicitante  socio que realiza la solicitud
      * @param acompanantes número de acompañantes que se aceptan
      * @brief Acepta una solicitud e indica el número de acompañantes
      */
@@ -115,7 +123,7 @@ public class Actividad {
     }
 
     public Solicitud buscarSolicitud(String solicitante) {
-        for (Solicitud solicitud: solicitudesPendientes) {
+        for (Solicitud solicitud : solicitudesPendientes) {
             if (solicitud.getSolicitante().getEmail() == solicitante)
                 return solicitud;
         }
@@ -130,9 +138,9 @@ public class Actividad {
     public void borrarSolicitud(String solicitudEmail) {
         if (solicitudesAceptadas.containsKey(solicitudEmail)) {
             solicitudesAceptadas.remove(solicitudEmail);
-        }else{
-            for(Solicitud sol: solicitudesPendientes){
-                if(solicitudEmail == sol.getSolicitante().getEmail()){
+        } else {
+            for (Solicitud sol : solicitudesPendientes) {
+                if (solicitudEmail == sol.getSolicitante().getEmail()) {
                     solicitudesPendientes.remove(sol);
                     break;
                 }
@@ -157,8 +165,9 @@ public class Actividad {
      * @param email         email del socio que realiza la solicitud
      * @param nAcompanantes número de acompañantes
      * @brief modifica el número de acompañantes que tendrá una solicitud
+     * @throws SolicitudNoValida en caso de que la solicitud no sea válida
      */
-    public void modificarAcompanantes(String email,int nAcompanantes) {
+    public void modificarAcompanantes(String email, int nAcompanantes) {
         if (solicitudesAceptadas.containsKey(email)) {
             solicitudesAceptadas.get(email).modificarAcompanantes(nAcompanantes);
         } else {
@@ -167,14 +176,20 @@ public class Actividad {
     }
 
     /**
-     *
-     * @return título de la actividad
+     * @brief Comprueba si las fechas de la actividad son válidas
+     * @throws FechaNoValida en caso de que las fechas no sean válidas
      */
+    public void fechasValidas() {
+        if (fechaCelebracion.isBefore(fechaInicioInscripcion) || fechaCelebracion.isBefore(fechaFinInscripcion)
+                || fechaInicioInscripcion.isAfter(fechaFinInscripcion))
+            throw new FechaNoValida();
+    }
+
     public String getTitulo() {
         return titulo;
     }
 
-    public void setPlazas(int  plazas) {
+    public void setPlazas(int plazas) {
         this.plazas = plazas;
     }
 
@@ -190,4 +205,7 @@ public class Actividad {
         return fechaFinInscripcion;
     }
 
+    public void setId(int i) {
+        this.id = i;
+    }
 }
