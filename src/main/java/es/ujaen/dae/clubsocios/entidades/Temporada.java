7852bd661fb795ba5904f3a2103109ef2479b94e
Francisco Javier Jiménez Aznar <fjja0004@red.ujaen.es>
2024-10-30 18:41:49 +0100

    Correciones menores

--- src/main/java/es/ujaen/dae/clubsocios/entidades/Temporada.java
+++ src/main/java/es/ujaen/dae/clubsocios/entidades/Temporada.java
@@ -3,6 +3,7 @@ package es.ujaen.dae.clubsocios.entidades;
 import es.ujaen.dae.clubsocios.excepciones.NoHayActividades;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,8 +13,15 @@ public class Temporada {
     private List<Actividad> actividades;
 
     /**
-     * @brief Constructor parametrizado de la clase Temporada
+     * @brief Constructor por defecto de la clase Temporada
+     */
+    public Temporada() {
+        actividades = new LinkedList<>();
+    }
+
+    /**
      * @param anio año de la temporada
+     * @brief Constructor parametrizado de la clase Temporada
      */
     public Temporada(int anio) {
         this.anio = anio;
@@ -21,17 +29,18 @@ public class Temporada {
     }
 
     /**
-     * @brief Crear una actividad, si es válida
      * @param actividad actividad a crear
+     * @brief Crear una actividad, si es válida
      */
     public void crearActividad(@Valid Actividad actividad) {
+
         actividades.add(actividad);
     }
 
     /**
-     * @brief Buscar una actividad por su título
      * @param titulo título de la actividad
      * @return la actividad con el título dado
+     * @brief Buscar una actividad por su título
      */
     public Actividad buscarActividadPorTitulo(String titulo) {
         for (Actividad actividad : actividades) {
@@ -43,9 +52,9 @@ public class Temporada {
     }
 
     /**
-     * @brief Devuelve una lista con todas las actividades a las que es posible inscribirse
      * @return lista de todas las actividades abiertas
      * @throws NoHayActividades si no hay actividades abiertas
+     * @brief Devuelve una lista con todas las actividades a las que es posible inscribirse
      */
     public List<Actividad> buscaTodasActividadesAbiertas() {
         List<Actividad> actividadesAbiertas = new LinkedList<>();
