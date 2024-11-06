ea190e1e226dcef6d33c950e6a71b3c0e779d6e4
Francisco Javier Jiménez Aznar <fjja0004@red.ujaen.es>
2024-11-06 17:08:58 +0100

    Correcciones en la clase Temporada
    Se ha eliminado la estructura que almacenaba las actividades, así como los métodos relacionados con ella.
    Ahora al atributo año se le asigna el año actual en el constructor por defecto.

--- src/main/java/es/ujaen/dae/clubsocios/entidades/Temporada.java
+++ src/main/java/es/ujaen/dae/clubsocios/entidades/Temporada.java
@@ -4,63 +4,28 @@ import es.ujaen.dae.clubsocios.excepciones.NoHayActividades;
 import es.ujaen.dae.clubsocios.objetosValor.Actividad;
 import jakarta.validation.constraints.Positive;
 
+import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Temporada {
     @Positive
     private int anio;
-    private List<Actividad> actividades;
 
     /**
      * @brief Constructor por defecto de la clase Temporada
      */
     public Temporada() {
-        actividades = new LinkedList<>();
+        this.anio = LocalDate.now().getYear();
     }
 
     /**
      * @param anio año de la temporada
-     * @brief Constructor parametrizado de la clase Temporada
      * @param anio año de la temporada
+     * @brief Constructor parametrizado de la clase Temporada
      */
     public Temporada(int anio) {
         this.anio = anio;
-        actividades = new LinkedList<>();
-    }
-
-//    /**
-//     * @brief Crear una actividad, si es válida.
-//     * @param actividad actividad a crear.
-//     * @exception ActividadYaExistente Lanza una excepción si ya existe la actividad.
-//     */
-//    public void crearActividad(@Valid Actividad actividad) {
-//
-//        try {
-//            if (buscarActividadPorTitulo(actividad.getTitulo()) != null){
-//                throw new ActividadYaExistente();
-//            }
-//        } catch (NoHayActividades ignored) {}
-//
-//        actividades.add(actividad);
-//    }
-
-    /**
-     * @brief Buscar una actividad por su título
-     * @param titulo título de la actividad
-     * @return la actividad con el título dado
-     * @exception NoHayActividades lanza una excepción si no existe ninguna actividad.
-     */
-    public Actividad buscarActividadPorTitulo(String titulo) {
-        if (actividades.isEmpty()){
-            throw new NoHayActividades();
-        }
-        for (Actividad actividad : actividades) {
-            if (actividad.getTitulo().equals(titulo)) {
-                return actividad;
-            }
-        }
-        return null;
     }
 
     public int getAnio() {
