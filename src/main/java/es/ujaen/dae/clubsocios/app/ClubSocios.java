059841ce953c4a2211c2b2beef1a466b397da547
Javier Fernández Basso <jfb00009@red.ujaen.es>
2024-11-13 16:10:47 +0100

    corrección de error hikari y añadimos el archivo de la base de datos

--- src/main/java/es/ujaen/dae/clubsocios/app/ClubSocios.java
+++ src/main/java/es/ujaen/dae/clubsocios/app/ClubSocios.java
@@ -1,10 +1,13 @@
 package es.ujaen.dae.clubsocios.app;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages="es.ujaen.dae.clubsocios.*")
+@EntityScan(basePackages = "src.main.java.entidades")
 @EnableScheduling
 public class ClubSocios {
     public static void main(String[] args) {
