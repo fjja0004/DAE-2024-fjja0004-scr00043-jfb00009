53a80bf5c62011b077847e4a2bd8e0aa4caf2a29
Silvia Cruz Román <scr00043@red.ujaen.es>
2024-11-19 20:15:13 +0100

    inicio repositoio temporadas

--- src/main/java/es/ujaen/dae/clubsocios/repositorios/RepositorioTemporadas.java
+++ src/main/java/es/ujaen/dae/clubsocios/repositorios/RepositorioTemporadas.java
@@ -1,8 +1,15 @@
 package es.ujaen.dae.clubsocios.repositorios;
 
+import es.ujaen.dae.clubsocios.entidades.Socio;
 import es.ujaen.dae.clubsocios.entidades.Temporada;
+import es.ujaen.dae.clubsocios.excepciones.SocioYaRegistrado;
 import es.ujaen.dae.clubsocios.excepciones.TemporadaYaExistente;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.List;
@@ -11,13 +18,30 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Repository
+@Transactional
 public class RepositorioTemporadas {
 
-    private Map<Integer, Temporada> temporadas;
-//TODO buscar todas las actividades de la temporada
-    public RepositorioTemporadas() {
-        temporadas = new HashMap<>();
+@PersistenceContext
+    EntityManager em;
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+public Optional<Temporada> buscar(int anio){
+    return Optional.ofNullable(em.find(Temporada.class,anio));
+}
+
+    public void guardar(Temporada temporada){
+
+        if (buscar(temporada.getAnio()).isPresent())
+            throw new TemporadaYaExistente();
+        em.persist(temporada);
     }
+
+
+
+    //private Map<Integer, Temporada> temporadas;
+//TODO buscar todas las actividades de la temporada
+    /*public RepositorioTemporadas() {
+        temporadas = new HashMap<>();
+    }*/
 
     /**
      * @brief Crea una nueva temporada
