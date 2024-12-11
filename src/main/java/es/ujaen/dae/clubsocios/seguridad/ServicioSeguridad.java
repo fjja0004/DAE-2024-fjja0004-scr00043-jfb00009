021a1273d357d72158ef1d5f84484cc2dd0791b6
Silvia Cruz Román <scr00043@red.ujaen.es>
2024-12-11 12:01:48 +0100

    Servicio Seguridad + añadir en el pom la dep de seguridad

--- src/main/java/es/ujaen/dae/clubsocios/seguridad/ServicioSeguridad.java
+++ src/main/java/es/ujaen/dae/clubsocios/seguridad/ServicioSeguridad.java
@@ -1,4 +1,36 @@
 package es.ujaen.dae.clubsocios.seguridad;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+
+@Configuration
 public class ServicioSeguridad {
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.disable())
+                .httpBasic(httpBasic -> httpBasic.realmName("club"))
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(HttpMethod.GET, "/socios/{email}")
+                        .access(new WebExpressionAuthorizationManager("hasRole('DIRECCION') or (hasRole('SOCIO') and #email == principal.username)"))
+                        .requestMatchers(HttpMethod.POST, "/actividades").hasRole("DIRECCION")
+                        .requestMatchers("/club/**").permitAll()
+                )
+                .build();
+
+
+    }
 }
